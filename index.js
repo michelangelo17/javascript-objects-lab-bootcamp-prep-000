61f603b30268c293fc8d8897a83feb1da9ce519a
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-14 13:31:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipeParts = [
+  'Ingredients',
+  'Directions'
+]
+var recipes = new Object({
+  Pizza: [recipeParts],
+  Burrito: [recipeParts],
+  Hotdog: [recipeParts]
+})
+
+function updateObjectWithKeyAndValue(obj, key, val) {
+  
+}
